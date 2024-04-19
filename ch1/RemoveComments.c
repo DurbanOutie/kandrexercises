4b3c0ba579c2955cae40e425888af086a8f9a0b9
durbanoutie <durbanout13@gmail.com>
2024-04-19 16:46:52 +0200

    Cleaned up RemoveComments.c

--- ch1/RemoveComments.c
+++ ch1/RemoveComments.c
@@ -1,12 +1,11 @@
-
 #include <stdio.h>
 
 /* Exercise 1-23. Write a program to remove all comments 
  * from a C program. Don't forget to handle quoted strings 
  * and character constants properly. 
- *
- * C comments don't nest.
  */
+// C comments don't nest.
+ 
 
 
 #define OUT_COMMENT 0
@@ -14,35 +13,19 @@
 #define IN_BLOCK    2
 #define IN_LINE     3
 
-
-
-/*
- * This is also a comment
- */
-
-
 int main(){
 
     int c, prevC;
     int state, maybe;
-
-    /*
-     * This is also a comment
-     */
 
     state = OUT_COMMENT;
     maybe = OUT_COMMENT;
     prevC = 0;
 
     while((c = getchar()) != EOF){
-
-        /*
-         * This is also a comment
-         */
-
         if(state == OUT_COMMENT){
             if(maybe == IN_COMMENT){
-                if(c == '*'){ //ssss
+                if(c == '*'){
                     state = IN_BLOCK;
                 }else if(c == '/'){
                     state = IN_LINE;
@@ -52,7 +35,7 @@ int main(){
                     maybe = OUT_COMMENT;
                 }
             }else if(maybe == OUT_COMMENT){
-                if(c == '/'){ // First Comment Start Char
+                if(c == '/'){
                     maybe = IN_COMMENT;
                 }else{
                     putchar(c);
@@ -60,38 +43,24 @@ int main(){
             }
         }else if(state == IN_BLOCK){
             if(maybe == OUT_COMMENT){
-                if(c != '/'){// Second Comment End Char
-                    //putchar(prevC);
-                    //putchat(c);
+                if(c != '/'){
                     maybe = IN_COMMENT;
                 }else{
                     state = OUT_COMMENT;
                 }
             }else if(maybe == IN_COMMENT){
-                if(c == '*'){ // First Comment End Char
+                if(c == '*'){
                     maybe = OUT_COMMENT;
                 }else{
-                    //putchar(c);
                 }
             }
-
         }else if(state == IN_LINE){
-
-
-
-
-
-
             if(c == '\n'){
                 state = OUT_COMMENT;
                 maybe = OUT_COMMENT;
                 putchar(c);
-            }else{
-
             }
         }
-
         prevC = c;
-
     }
 }
